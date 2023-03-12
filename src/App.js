5751e6c73bead81806aee072186c5b18dda2b204
Ryota <studyprograming51@gmail.com>
2023-03-12 15:57:42 +0900

    change:sredlistcreate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
 import './App.css';
+import { useState } from 'react';
+import { sredData } from './sredData';
 
 function App() {
+const initialsred = {
+  value: sredData,
+}
+
+const [sred,setsred] = useState(initialsred)
+
   return (
     <div className="App">
-      
 
+      <div className='boardtitle'>
+        <h1>新着スレッド</h1>
+        <hr/>
+      </div>
+
+      <div className='sreddisplay'>
+        {sred.value.map((sred)=>(
+        <div key = {sred.id} className='sred'>
+          <div className='sredName'>
+            <h1>{sred.name}</h1>
+          </div>
+        </div>))}
+        
+      </div>
     </div>
   );
 }
